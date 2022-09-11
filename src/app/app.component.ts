bf80a26beb9aac1eccdabe5ece3ace67bc20bba7
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2022-09-11 20:15:03 +0800

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+
+  // template: `<h1>Hello World from inline template!</h1>
+  // Angular is Awesome.`,
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'hotelinventoryapp';
+}
