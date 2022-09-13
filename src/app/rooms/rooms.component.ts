70b49cf1315e9340f3a1c2d92f60a49cd6cb3494
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2022-09-13 22:46:53 +0800

    Binding Syntax - 03 Event Binding

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -9,10 +9,14 @@ export class RoomsComponent implements OnInit {
 
   hotelName :string = 'Lourd Hotel';
   numberOfRooms : Number = 10;
-  
+
+  hideRooms : boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggle(){
+    this.hideRooms =!this.hideRooms;
+  }
 }
